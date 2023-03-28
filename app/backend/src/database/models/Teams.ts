e870ddad9057b7564f8f284888832a9697238945
LeandroDoudement <leandrodoudement@gmail.com>
2023-03-28 16:06:43 -0300

    Requisito 1 | Teams Model and Migration

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,29 @@
+import { STRING, INTEGER, Model } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+  },
+);
+
+export default Teams;
